7041d413dec76af9a8f5f8d9422fd8a656b64ea5
Tomas Carnecky <tomc@caurea.org>
2020-04-01 07:21:44 +0200

    Report write only when the content has changed

--- src/cli/stdlib/write.ts
+++ src/cli/stdlib/write.ts
@@ -5,16 +5,20 @@ export async function write(path: string, content0: string, force: boolean = fal
   const content = content0.trim() + "\n";
   const ps = kleur.gray("".padEnd(Math.max(0, 70 - path.length), "."));
 
-  if (force) {
-    console.log(`  - ${path} ${ps} ${kleur.red("update")}`);
-    await fs.promises.writeFile(path, content);
-  } else {
-    try {
-      await fs.promises.stat(path);
-      console.log(`  - ${path} ${ps} ${kleur.gray("skip")}`);
-    } catch (err) {
-      console.log(`  - ${path} ${ps} ${kleur.green("write")}`);
+  const originalContent = await fs.promises.readFile(path, "utf8").catch(() => "");
+
+  if (originalContent !== content) {
+    if (force) {
+      console.log(`  - ${path} ${ps} ${kleur.red("update")}`);
       await fs.promises.writeFile(path, content);
+    } else {
+      try {
+        await fs.promises.stat(path);
+        console.log(`  - ${path} ${ps} ${kleur.gray("skip")}`);
+      } catch (err) {
+        console.log(`  - ${path} ${ps} ${kleur.green("write")}`);
+        await fs.promises.writeFile(path, content);
+      }
     }
   }
 }
