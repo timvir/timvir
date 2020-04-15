34fb1be60b9c13c78d6e2d31f3f59bd8621475e0
Tomas Carnecky <tomc@caurea.org>
2020-04-15 10:12:08 +0200

    Include Viewport in @timvir/blocks

--- src/packages/blocks/index.ts
+++ src/packages/blocks/index.ts
@@ -4,3 +4,4 @@ export { Exhibit } from "../../components/Exhibit";
 export { Grid } from "../../components/Grid";
 export { Message } from "../../components/Message";
 export { Swatch } from "../../components/Swatch";
+export { Viewport } from "../../components/Viewport";
