5d39113109b8b0ce3519f4d982103b396ef5db56
Tomas Carnecky <tomc@caurea.org>
2020-04-23 01:06:30 +0200

    extendedWidth Page layout option

--- src/components/Page/index.ts
+++ src/components/Page/index.ts
@@ -1,4 +1,4 @@
-export { fullWidth } from "./layout";
+export { extendedWidth, fullWidth } from "./layout";
 export { default as Page } from "./Page";
 export { theme } from "./theme";
 export * from "./types";
--- src/components/Page/layout.tsx
+++ src/components/Page/layout.tsx
@@ -6,18 +6,27 @@ export const grid = css`
   display: grid;
 
   grid-auto-rows: min-content;
-  grid-template-columns: [le] 0 [lc] 1fr [rc] 0 [re];
-  grid-column-gap: 16px;
+  grid-template-columns: [le] 16px [lex lc] 1fr [rc rex] 16px [re];
 
-  @media (min-width: 60rem) {
-    grid-template-columns: [le] 1fr [lc] minmax(0, 48rem) [rc] 1fr [re];
-    grid-column-gap: 24px;
+  @media (min-width: 48rem) {
+    grid-template-columns: [le] 24px [lex] 1fr [lc] minmax(0, 48rem) [rc] 1fr [rex] 24px [re];
+  }
+
+  @media (min-width: 72rem) {
+    grid-template-columns: [le] 1fr 24px [lex] minmax(0, 12rem) [lc] 48rem [rc] minmax(0, 12rem) [rex] 24px 1fr [re];
   }
 
   & > *:not(.${noLayout}) {
     grid-column: lc / rc;
   }
 `;
+
+export const extendedWidth = cx(
+  noLayout,
+  css`
+    grid-column: lex / rex;
+  `
+);
 
 export const fullWidth = cx(
   noLayout,
