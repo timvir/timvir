10bb5b3461ef545b32d9cfa55b9aed2416f493bd
Tomas Carnecky <tomc@caurea.org>
2020-08-02 18:00:35 +0200

    Move viewport meta tag to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,26 @@
+import Head from "next/head";
 import React from "react";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <style jsx global>{`
-      body {
-        margin: 0;
-        box-sizing: border-box;
-        overscroll-behavior: none;
-      }
+        body {
+          margin: 0;
+          box-sizing: border-box;
+          overscroll-behavior: none;
+        }
 
-      *,
-      *:before,
-      *:after {
-        box-sizing: inherit;
-      }
-    `}</style>
+        *,
+        *:before,
+        *:after {
+          box-sizing: inherit;
+        }
+      `}</style>
+
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no" />
+      </Head>
 
       <Component {...pageProps} />
     </>
