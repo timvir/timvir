6d42a41831e9792ddb8de2a6f00385226e588adc
Tomas Carnecky <tomc@caurea.org>
2021-09-07 16:49:00 +0200

    Point linaria cache directory to inside .next

--- next.config.js
+++ next.config.js
@@ -1,79 +1,13 @@
 const withPlugins = require("next-compose-plugins");
-
-/*
- * withLinaria()
- */
-function withLinaria(nextConfig) {
-  const LINARIA_EXTENSION = ".linaria.module.css";
-
-  function traverse(rules) {
-    for (let rule of rules) {
-      if (typeof rule.loader === "string" && rule.loader.includes("css-loader")) {
-        if (rule.options && rule.options.modules && typeof rule.options.modules.getLocalIdent === "function") {
-          let nextGetLocalIdent = rule.options.modules.getLocalIdent;
-          rule.options.modules.getLocalIdent = (context, _, exportName, options) => {
-            if (context.resourcePath.includes(LINARIA_EXTENSION)) {
-              return exportName;
-            }
-            return nextGetLocalIdent(context, _, exportName, options);
-          };
-        }
-      }
-      if (typeof rule.use === "object") {
-        traverse(Array.isArray(rule.use) ? rule.use : [rule.use]);
-      }
-      if (Array.isArray(rule.oneOf)) {
-        traverse(rule.oneOf);
-      }
-    }
-  }
-
-  return {
-    ...nextConfig,
-    webpack(config, options) {
-      traverse(config.module.rules);
-
-      config.module.rules.push({
-        test: /(?!_app)\.(tsx|ts|js|mjs|jsx)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: require.resolve("@linaria/webpack-loader"),
-            options: {
-              sourceMap: process.env.NODE_ENV !== "production",
-              ...(nextConfig.linaria || {}),
-              extension: LINARIA_EXTENSION,
-            },
-          },
-        ],
-      });
-      config.module.rules.push({
-        test: /_app\.(tsx|ts|js|mjs|jsx)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: require.resolve("@linaria/webpack-loader"),
-            options: {
-              sourceMap: process.env.NODE_ENV !== "production",
-              ...(nextConfig.linaria || {}),
-              extension: ".css",
-            },
-          },
-        ],
-      });
-
-      if (typeof nextConfig.webpack === "function") {
-        return nextConfig.webpack(config, options);
-      }
-
-      return config;
-    },
-  };
-}
+const withLinaria = require("next-linaria");
 
 module.exports = withPlugins([require("@next/mdx")({ extension: /\.mdx?$/ }), withLinaria], {
   future: {
     webpack5: true,
+  },
+
+  linaria: {
+    cacheDirectory: "./.next/cache/linaria",
   },
 
   productionBrowserSourceMaps: true,
