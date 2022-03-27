4f1edb31ce438a0f5c1193e535873fdc48c39736
Tomas Carnecky <tomc@caurea.org>
2022-03-27 15:33:35 +0200

    Show code on ColorBar page

--- pkg/blocks/ColorBar/samples/basic.tsx
+++ pkg/blocks/ColorBar/samples/basic.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { ColorBar } from "..";
+import { ColorBar } from "@timvir/blocks";
 
 export default function Sample() {
   return <ColorBar values={["#97F3EB", "#78D5CC", "#58B8AE", "#369C91", "#008075"]} />;
