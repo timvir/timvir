c7f96656af98567fb65d5a9685e9c291b8be94f2
Tomas Carnecky <tomc@caurea.org>
2022-04-23 16:33:28 +0000

    Try bus in Code docs

---
+++ pkg/blocks/Code/samples/toggle.tsx
@@ -0,0 +1,51 @@
+import { useMDXComponents } from "@mdx-js/react";
+import { useContext } from "@timvir/core";
+import { send } from "@timvir/core/bus";
+import * as React from "react";
+
+export default function Sample({ language, children }: any) {
+  const context = useContext();
+
+  const components = { a: "a", ...useMDXComponents() };
+
+  return (
+    <components.a
+      href="#"
+      role="button"
+      onClick={(ev: React.SyntheticEvent) => {
+        ev.preventDefault();
+        send(context.bus, "code", "merge", {
+          highlightedLines: undefined,
+          language,
+          children: code[language as any as keyof typeof code],
+          caption: null,
+        });
+      }}
+    >
+      {children}
+    </components.a>
+  );
+}
+
+const code = {
+  javascript: "function foo() {\n  const answer = 42;\n}",
+  markdown: "# Lorem\n\nipsum dolor",
+  html: `<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First Heading</h1>
+
+<p>My first paragraph.</p>
+
+</body>
+</html>
+  `,
+  haskell: `primes = filterPrime [2..]
+where filterPrime (p:xs) =
+        p : filterPrime [x | x <- xs, x \`mod\` p /= 0]`,
+  css: `.gwbiq5k>:not(.nnbogi4) {
+    grid-column: lc/rc;
+    min-width: 0;
+}`,
+};
