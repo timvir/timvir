3eccff4358d20af8becccc3746c9bb65a5e268d8
Tomas Carnecky <tomc@caurea.org>
2022-04-24 00:31:56 +0200

    Clean up styling of Arbitrary

--- pkg/blocks/Arbitrary/Arbitrary.tsx
+++ pkg/blocks/Arbitrary/Arbitrary.tsx
@@ -1,10 +1,10 @@
-import { css } from "@linaria/core";
+import { css, cx } from "@linaria/core";
 import { Exhibit } from "@timvir/blocks";
+import { useBlock } from "@timvir/core";
+import * as base58 from "@timvir/std/base58";
 import * as React from "react";
 import { useImmer } from "use-immer";
-import * as base58 from "@timvir/std/base58";
 import { Context } from "./context";
-import { useBlock } from "@timvir/core";
 
 /**
  * The underlying DOM element which is rendered by this component.
@@ -16,9 +16,9 @@ interface Props extends React.ComponentPropsWithRef<typeof Root> {
 }
 
 function Arbitrary(props: Props, ref: React.ForwardedRef<React.ElementRef<typeof Root>>) {
-  const block = useBlock(props)
+  const block = useBlock(props);
 
-  const { ExhibitProps, children, ...rest } = block.props;
+  const { ExhibitProps, className, children, ...rest } = block.props;
 
   const [value, mutate] = useImmer({
     seed: 0,
@@ -32,66 +32,12 @@ function Arbitrary(props: Props, ref: React.ForwardedRef<React.ElementRef<typeof
 
   return (
     <Context.Provider value={value}>
-      <Root ref={ref} style={{ margin: "1em 0" }} {...rest}>
-        <div
-          className={css`
-            display: grid;
-            grid-gap: 12px;
-            grid-template-columns: 1fr 100px;
-            align-items: center;
-            margin-bottom: 8px;
-          `}
-        >
-          <div
-            className={css`
-              display: flex;
-              align-items: center;
-              padding: 0 0 0 10px;
-              height: 36px;
-              position: relative;
-
-              &::after {
-                display: block;
-                position: absolute;
-                content: "";
-                inset: 0;
-                pointer-events: none;
-                border: 1px solid var(--c-p-2);
-                border-radius: 2px;
-              }
-
-              &:hover::after {
-                border-color: var(--c-p-4);
-              }
-
-              &:focus-within::after {
-                border: 2px solid var(--c-p-4);
-                border-radius: 3px;
-              }
-            `}
-          >
-            <span
-              className={css`
-                display: inline-block;
-                color: var(--timvir-secondary-text-color);
-                margin-right: 6px;
-              `}
-            >
-              Seed:
-            </span>
-
+      <Root ref={ref} className={cx(classes.root, className)} {...rest}>
+        <div className={classes.controls}>
+          <div className={classes.textField}>
+            <span className={classes.startAdornment}>Seed:</span>
             <input
-              className={css`
-                border: none;
-                outline: none;
-                font: inherit;
-                background: transparent;
-                align-self: stretch;
-                padding: 0;
-                height: 34px;
-                width: 100%;
-                color: inherit;
-              `}
+              className={classes.input}
               placeholder="Seed"
               value={base58.encode(new TextEncoder().encode(`${value.seed}`))}
               onPaste={(ev) => {
@@ -107,23 +53,7 @@ function Arbitrary(props: Props, ref: React.ForwardedRef<React.ElementRef<typeof
           </div>
 
           <button
-            className={css`
-              border: none;
-              outline: none;
-              height: 36px;
-              background: var(--c-p-2);
-              font: inherit;
-              border-radius: 2px;
-              cursor: pointer;
-              transition: background 0.2s;
-
-              &:hover {
-                background: var(--c-p-3);
-              }
-              &:active {
-                background: var(--c-p-4);
-              }
-            `}
+            className={classes.button}
             onClick={() => {
               mutate((draft) => {
                 draft.seed = crypto.getRandomValues(new Uint32Array(1))[0];
@@ -133,6 +63,7 @@ function Arbitrary(props: Props, ref: React.ForwardedRef<React.ElementRef<typeof
             Refresh
           </button>
         </div>
+
         <Exhibit {...ExhibitProps}>{children}</Exhibit>
       </Root>
     </Context.Provider>
@@ -140,3 +71,78 @@ function Arbitrary(props: Props, ref: React.ForwardedRef<React.ElementRef<typeof
 }
 
 export default React.forwardRef(Arbitrary);
+
+const classes = {
+  root: css`
+    margin: 1em 0;
+  `,
+
+  controls: css`
+    display: grid;
+    grid-gap: 8px;
+    grid-template-columns: 1fr 100px;
+    margin-bottom: 8px;
+  `,
+
+  textField: css`
+    display: flex;
+    align-items: center;
+    padding: 0 0 0 10px;
+    height: 36px;
+    position: relative;
+
+    &::after {
+      display: block;
+      position: absolute;
+      content: "";
+      inset: 0;
+      pointer-events: none;
+      border: 1px solid var(--timvir-secondary-text-color);
+      border-radius: 2px;
+    }
+
+    &:hover::after {
+      border-color: var(--timvir-accent-color);
+    }
+
+    &:focus-within::after {
+      border-color: var(--timvir-accent-color);
+    }
+  `,
+
+  startAdornment: css`
+    display: inline-block;
+    color: var(--timvir-secondary-text-color);
+    margin-right: 6px;
+  `,
+
+  input: css`
+    border: none;
+    outline: none;
+    font: inherit;
+    background: transparent;
+    align-self: stretch;
+    padding: 0;
+    height: 34px;
+    width: 100%;
+    color: inherit;
+  `,
+
+  button: css`
+    border: none;
+    outline: none;
+    height: 36px;
+    background: var(--timvir-accent-color);
+    font: inherit;
+    border-radius: 2px;
+    cursor: pointer;
+    transition: background 0.2s;
+
+    &:hover {
+      background: var(--c-p-3);
+    }
+    &:active {
+      background: var(--c-p-4);
+    }
+  `,
+};
