047b91ec05bef947f55d4e1ea106f523d50483a5
Tomas Carnecky <tomc@caurea.org>
2022-05-29 18:35:15 +0200

    Prettify Nix shell

--- shell.nix
+++ shell.nix
@@ -1,9 +1,31 @@
 let
   pkgs = import <nixpkgs> {};
 
+  banner = pkgs.writeShellScriptBin "banner" ''
+    clear
+
+    ${pkgs.figlet}/bin/figlet timvir
+    echo "     [timˈvir] n. book"
+  '';
+
+  tools = {
+    dev = pkgs.writeShellScriptBin "dev" ''
+      clear
+      ./node_modules/.bin/next dev
+    '';
+  };
+
 in pkgs.mkShell {
   buildInputs = [
     pkgs.nodejs-16_x
     pkgs.jq
+
+    tools.dev
   ];
+
+  shellHook = ''
+    if [ -n "$PS1" ]; then
+      ${banner}/bin/banner
+    fi
+  '';
 }
