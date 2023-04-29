9dff92f90cb7fca3b09576d9dacec7d82c1b7787
Tomas Carnecky <tomc@caurea.org>
2023-04-29 14:53:53 +0200

    Remove Live component

--- src/components/Live/samples/basic.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { Live } from "..";
-
-type Props = Partial<React.ComponentPropsWithoutRef<typeof Live>>;
-
-export default function Sample(props: Props) {
-  return <Live {...props} />;
-}
