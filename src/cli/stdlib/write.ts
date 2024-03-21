8f26e047a5eb2987050c94dcec5967b106d2baf7
Tomáš Čarnecký <tomc@caurea.org>
2024-03-21 10:18:29 +0100

    Check if directory exists before readdir

--- src/cli/stdlib/write.ts
+++ src/cli/stdlib/write.ts
@@ -3,7 +3,7 @@ import kleur from "kleur";
 
 export async function write(path: string, content0: string, force = false): Promise<void> {
   const content = content0.trim() + "\n";
-  const ps = kleur.gray("".padEnd(Math.max(0, 70 - path.length), "."));
+  const ps = kleur.gray("".padEnd(Math.max(0, 90 - path.length), "."));
 
   const originalContent = await fs.promises.readFile(path, "utf8").catch(() => "");
 
