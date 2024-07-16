e16c865d9ea48c476f4bd1de181cdbe657e67ed3
Tomáš Čarnecký <tomc@caurea.org>
2024-07-16 21:58:17 +0200

    Improve Exhibit documentation

---
+++ pkg/timvir/blocks/Exhibit/samples/usage.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Exhibit } from "..";
+
+declare const YourReactComponent: any
+
+export default function Sample() {
+  return (
+    <Exhibit caption="Here goes your caption">
+      <YourReactComponent />
+    </Exhibit>
+  );
+}
