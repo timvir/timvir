24777a7237e7a46c7cdc0b8e718fe475416662b5
Tomas Carnecky <tomc@caurea.org>
2024-07-20 14:02:12 +0200

    Remove Image component

--- src/components/Image/samples/basic.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { Image } from "..";
-
-import image from "../../../../assets/khachik-simonian-nXOB-wh4Oyc-unsplash.jpg";
-
-export default function Sample() {
-  return <Image metadata={image} img={{ src: image.src }} sources={[]} />;
-}
