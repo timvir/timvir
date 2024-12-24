29c9f9376971a058940f28d6bb4ec4402a52999f
Tomas Carnecky <tomc@caurea.org>
2024-12-24 15:30:32 +0100

    Remove useRouter() from samples

--- pkg/timvir/core/components/Page/samples/basic.tsx
+++ pkg/timvir/core/components/Page/samples/basic.tsx
@@ -1,8 +1,7 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
 import * as React from "react";
 import { Page } from "..";
 
 export default function Sample() {
-  return <Page toc={[]} Link={Link as any} location={useRouter()} />;
+  return <Page toc={[]} Link={Link as any} location={{ asPath: "/", push: () => {} }} />;
 }
