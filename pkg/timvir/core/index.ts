08b72c608a4c86ecf05aac871f1d319c8950ecda
Tomas Carnecky <tomc@caurea.org>
2024-12-29 17:36:39 +0100

    .

--- pkg/timvir/core/index.ts
+++ pkg/timvir/core/index.ts
@@ -9,7 +9,7 @@ export * from "./components/Page";
 
 export { useContext } from "timvir/context";
 export { extendedWidth, fullWidth, grid } from "./layout";
-export { theme } from "./theme";
+// export { theme } from "./theme";
 
 /**
  * A mailbox is a wonka source which receives messages for one specific block (identified by its id).
