4391295b7380e1782485dd6b4debcf67187c3c85
Tomas Carnecky <tomc@caurea.org>
2025-07-02 20:45:17 +0200

    Migrate static MDX files to App Router

---
+++ src/app/blocks/Cover/page.tsx
@@ -0,0 +1,10 @@
+import Content from "../../../../pkg/timvir/blocks/Cover/docs/index.mdx";
+import Wrapper from "../../../timvir/wrapper2";
+
+export default function Page() {
+  return (
+    <Wrapper>
+      <Content />
+    </Wrapper>
+  );
+}
