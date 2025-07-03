d505c70a8b691f827e6936f0c31d3b744462d1ba
Tomas Carnecky <tomc@caurea.org>
2025-07-03 08:02:10 +0200

    Remove Next.js Pages Router

---
+++ src/app/docs/components/[component]/samples/[sample]/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { theme } from "timvir/core";
+
+interface Props {
+  params: Promise<{
+    component: string;
+    sample: string;
+  }>;
+}
+
+export default async function Page(props: Props) {
+  const { component, sample } = await props.params;
+
+  const Component = await (async () => {
+    if (component === "Page" || component === "Footer" || component === "NavigationFooter") {
+      return import(`../../../../../../../pkg/timvir/core/components/${component}/samples/${sample}.tsx`);
+    } else if (
+      [
+        "Arbitrary",
+        "Code",
+        "ColorBar",
+        "ColorBook",
+        "Cover",
+        "Exhibit",
+        "Font",
+        "Grid",
+        "Icon",
+        "Message",
+        "Swatch",
+        "Viewport",
+        "WebLink",
+      ].includes(component)
+    ) {
+      return import(`../../../../../../../pkg/timvir/blocks/${component}/samples/${sample}.tsx`);
+    } else {
+      return import(`../../../../../../components/${component}/samples/${sample}.tsx`);
+    }
+  })().then((mod) => mod.default || mod);
+
+  return (
+    <div className={theme}>
+      <Component />
+    </div>
+  );
+}
