e40a453eb0ff1b4781078efb38d6775840e203f5
Tomas Carnecky <tomc@caurea.org>
2025-07-06 08:35:38 +0200

    Drop unused components . .

--- src/app/blocks/[block]/page.tsx
+++ src/app/blocks/[block]/page.tsx
@@ -10,11 +10,7 @@ export default async function Page(props: Props) {
   const { block } = await props.params;
 
   const Component = await (async () => {
-    if (block === "Page" || block === "Footer" || block === "NavigationFooter") {
-      return import(`../../../../pkg/timvir/core/components/${block}/docs/index.mdx`);
-    } else {
-      return import(`../../../components/${block}/docs/index.mdx`);
-    }
+    return import(`../../../../pkg/timvir/core/components/${block}/docs/index.mdx`);
   })().then((mod) => mod.default || mod);
 
   return (
--- src/components/Figma/samples/basic.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { Figma } from "..";
-
-type Props = Partial<React.ComponentPropsWithoutRef<typeof Figma>>;
-
-export default function Sample(props: Props) {
-  return <Figma url="https://www.figma.com/file/0FenYNPAo5wV7cmYPELaUE" {...props} />;
-}
