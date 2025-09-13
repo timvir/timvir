ba46ee7ce8a48cb1f7d3f73c05e5b46ed58c107e
Tomáš Čarnecký <tomc@caurea.org>
2025-09-13 23:04:18 +0200

    Migrate ColorBar styling to StyleX

--- pkg/timvir/blocks/ColorBar/ColorBar.tsx
+++ pkg/timvir/blocks/ColorBar/ColorBar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { css, cx } from "@linaria/core";
+import { cx } from "@linaria/core";
+import * as stylex from "@stylexjs/stylex";
 import { Swatch } from "timvir/blocks";
 import { useBlock } from "timvir/core";
 import * as React from "react";
@@ -20,16 +21,29 @@ interface Props extends React.ComponentPropsWithoutRef<typeof Root> {
 function ColorBar(props: Props, ref: React.ForwardedRef<React.ComponentRef<typeof Root>>) {
   const block = useBlock(props);
 
-  const { values, className, ...rest } = block.props;
+  const { values, ...rest } = block.props;
 
   const [selected, setSelected] = React.useState<undefined | Props["values"][number]>(undefined);
 
+  const rootStyleProps = stylex.props(styles.root);
+
   return (
-    <Root ref={ref} className={cx(className, classes.root, selected && tweaks.selected)} {...rest}>
-      <div className={classes.bar} style={{ opacity: selected ? 0 : 1 }}>
-        {values.map((value, i) => (
-          <div key={i} className={classes.value}>
+    <Root
+      ref={ref}
+      {...rest}
+      {...rootStyleProps}
+      className={cx(rest.className, rootStyleProps.className)}
+      style={{ ...rootStyleProps.style, ...rest.style }}
+    >
+      <div {...stylex.props(styles.bar)} style={{ opacity: selected ? 0 : 1 }}>
+        {values.map((value, i, self) => (
+          <div key={i} {...stylex.props(styles.value)}>
             <div
+              {...stylex.props(
+                i === 0 && styles.firstChild,
+                i === self.length - 1 && styles.lastChild,
+                styles.valueInner
+              )}
               style={{ background: typeof value === "string" ? value : value.value }}
               onClick={() => {
                 setSelected(value);
@@ -39,98 +53,75 @@ function ColorBar(props: Props, ref: React.ForwardedRef<React.ComponentRef<typeo
         ))}
       </div>
 
-      <div className={classes.overlay}>
-        <Swatch
-          {...(typeof selected === "string" ? { value: selected } : { value: selected?.value ?? "" })}
-          onMouseLeave={() => {
-            setSelected(undefined);
-          }}
-        />
-      </div>
+      {selected !== undefined && (
+        <div {...stylex.props(styles.overlay)}>
+          <Swatch
+            {...(typeof selected === "string" ? { value: selected } : { value: selected.value })}
+            onMouseLeave={() => {
+              setSelected(undefined);
+            }}
+          />
+        </div>
+      )}
     </Root>
   );
 }
 
 export default React.forwardRef(ColorBar);
 
-const tweaks = {
-  selected: css``,
-};
+const styles = stylex.create({
+  root: {
+    position: "relative",
+  },
 
-const classes = {
-  root: css`
-    position: relative;
-  `,
+  bar: {
+    display: "grid",
+    gridAutoFlow: "row",
+    gridTemplateColumns: "repeat(auto-fit, minmax(0, 1fr))",
+  },
 
-  bar: css`
-    display: grid;
-    grid-auto-flow: row;
-    grid-template-columns: repeat(auto-fit, minmax(0, 1fr));
-    transition: all 0.16s;
+  value: {
+    height: 40,
+    flexGrow: 1,
 
-    &::before {
-      position: absolute;
-      top: 0;
-      right: 0;
-      bottom: 0;
-      left: 0;
-      display: block;
-      z-index: 2;
-      border-radius: 2px;
-      box-shadow: inset 0 0 0 1px rgba(16, 22, 26, 0.15);
-      content: "";
-      pointer-events: none;
-      user-select: none;
-      transition: all 0.16s;
-    }
-    &:hover::before {
-      opacity: 0;
-    }
-  `,
+    display: "grid",
+    placeItems: "stretch",
 
-  value: css`
-    height: 40px;
-    flex-grow: 1;
+    cursor: "pointer",
+    zIndex: 3,
 
-    display: grid;
-    place-items: stretch;
+    "--timvir-b-ColorBar-value-margin": "0px",
 
-    & > div {
-      transition: all 0.16s;
-      cursor: pointer;
-    }
-    &:first-child > div {
-      border-radius: 2px 0 0 2px;
-    }
-    &:last-child > div {
-      border-radius: 0 2px 2px 0;
-    }
+    ":hover": {
+      "--timvir-b-ColorBar-value-borderRadius": "2px",
+      "--timvir-b-ColorBar-value-margin": "-3px 1px",
+    },
+  },
 
-    &:hover {
-      z-index: 3;
-    }
-    &:hover > div {
-      border-radius: 2px;
-      margin: -3px 1px;
-      box-shadow: inset 0 0 0 1px rgba(16, 22, 26, 0.2), 0 2px 4px rgba(16, 22, 26, 0.1),
-        0 8px 24px rgba(16, 22, 26, 0.2);
-    }
-  `,
+  valueInner: {
+    transition: "all 0.16s",
 
-  overlay: css`
-    position: absolute;
-    top: 50%;
-    right: 0px;
-    left: 0px;
-    transform: translateY(-50%);
-    pointer-events: none;
-    opacity: 0;
-    z-index: 4;
-    transition: opacity 0.16s;
+    borderRadius: "var(--timvir-b-ColorBar-value-borderRadius, var(--timvir-b-ColorBar-value-borderRadiusDefault))",
+    margin: "var(--timvir-b-ColorBar-value-margin)",
 
-    .${tweaks.selected} & {
-      pointer-events: all;
-      opacity: 1;
-    }
-  `,
-};
+    ":hover": {
+      boxShadow:
+        "inset 0 0 0 1px rgba(16, 22, 26, 0.2), 0 2px 4px rgba(16, 22, 26, 0.1), 0 8px 24px rgba(16, 22, 26, 0.2)",
+    },
+  },
+  firstChild: {
+    "--timvir-b-ColorBar-value-borderRadiusDefault": "2px 0 0 2px",
+  },
+  lastChild: {
+    "--timvir-b-ColorBar-value-borderRadiusDefault": "0 2px 2px 0",
+  },
+
+  overlay: {
+    position: "absolute",
+    top: "50%",
+    right: 0,
+    left: 0,
+    transform: "translateY(-50%)",
+    zIndex: 4,
+  },
+});
