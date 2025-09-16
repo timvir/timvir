7a33b22f9dd5a82960d4c9179173c80c2b997e47
Tomas Carnecky <tomc@caurea.org>
2025-09-16 11:27:04 +0200

    Remove hack that forced CSS to emit in a specific order

--- pkg/timvir/builtins/index.ts
+++ pkg/timvir/builtins/index.ts
@@ -1,11 +1,1 @@
-/*
- * Hack to fix the order in which CSS is generated. The CSS defined
- * in layout needs to be emitted first.
- */
-import * as hack from "../core/layout";
-export const marquee = () => {
-  void hack;
-  return null;
-};
-
 export * from "./components";
